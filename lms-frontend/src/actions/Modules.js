0f22f4fd6fb29450c03bdea7ac6c5415930639c0
vidura umayanaga perera <laumayangaperera@gmail.com>
2021-08-14 15:06:15 +0530

    add actions of modules and wrote the api calls form the front end

---
+++ lms-frontend/src/actions/Modules.js
@@ -0,0 +1,53 @@
+import {FETCH_MODULE, FETCH_SINGLE_MODULE, CREATE_MODULE, UPDATE_MODULE, DELETE_MODULE} from "../constants/constant.js";
+import * as api from '../api/index.js';
+
+//getting all the modules
+export const getModules = () => async (dispatch) =>{
+    try{
+        const {data} = await api.fetchModules();
+        dispatch ({type: FETCH_MODULE, payload: data});
+
+    }catch (error){
+        console.log("getting workshop error" + error);
+    }
+}
+
+//getting a single module
+export const getSingleModule = (id) => async (dispatch) =>{
+    try{
+        const  {data} = await api.fetchSingleModule(id);
+        dispatch ({type: FETCH_SINGLE_MODULE, payload: data});
+
+    }catch (error){
+        console.log("getting single module error" + error)
+    }
+}
+
+//create a module
+export const createModules = (module) => async (dispatch) =>{
+    try{
+        const res = await api.createModules(module);
+        dispatch ({type: CREATE_MODULE, payload: res.data})
+        return{...res}
+    }catch (error){
+        console.log("create module error")
+    }
+}
+
+//remove single module
+export const removeModule = (id) => async (dispatch) =>{
+    try{
+        const res = await api.deleteModule(id);
+    }catch (error){
+        console.log("delete module" + error);
+    }
+}
+
+//update single module
+export const updateSingleModule = (module) => async (dispatch) =>{
+    try {
+        const res = await api.updateModules(module);
+    }catch (error){
+        console.log("update module" + error);
+    }
+}
