dd64e157c8407732b9af651e95457400a30c61f0
vidura <laumayangaperera@gmail.com>
2021-09-09 23:43:14 +0530

    add credits

--- lms-frontend/src/Components/module/CreateModuleForm.js
+++ lms-frontend/src/Components/module/CreateModuleForm.js
@@ -4,7 +4,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {createModules} from "../../actions/Modules";
 import {getUsers} from "../../actions/Users";
 import 'antd/dist/antd.css';
-import { DatePicker } from 'antd';
+import {DatePicker, message} from 'antd';
 
 let option_lec = [], option_lab = [];
 
@@ -12,14 +12,16 @@ const CreateModuleForm = () =>{
 
     const dispatch = useDispatch();
 
+    const [loadingBtn, setLoadingBtn] = useState(false);
     //initial data in the Form
     const [moduleData, setModuleData] = useState({
         name: '',
         module_code: '',
-        lecture_in_charge: undefined,
+        lecturer_in_charge: undefined,
         lab_assistant: undefined,
         year: '',
-        semester:''
+        semester:'',
+        credit:''
     })
 
     //calling Users to the Component
@@ -29,27 +31,37 @@ const CreateModuleForm = () =>{
     const userData = useSelector((state) => state.UserReducer.users)
 
     //filter labInstructor from the UserData
-    option_lab = userData?.filter((user) => user.role === "Lab Instructor").map((lab) => ({
+    option_lab = userData?.filter((user) => user.role === "labInstructor").map((lab) => ({
         value: lab._id, label: lab.name}))
 
     //filter lecturer from the UserData
-    option_lec = userData?.filter((user)=> user.role === "Lecturer").map((lec) =>({
+    option_lec = userData?.filter((user)=> user.role === "lecturer").map((lec) =>({
         value:lec._id, label: lec.name}))
 
 
     //Passing Module Data to the Database
     const handleSubmit = async (e) =>{
         e.preventDefault();
+        setLoadingBtn(true)
         const passData ={
             name: moduleData.name,
             module_code:moduleData.module_code,
-            lecture_in_charge:moduleData.lecture_in_charge,
-            lab_assistant:moduleData.lab_assistant,
+            lecturer_in_charge:moduleData.lecturer,
+            lab_assistant:moduleData.labInstructor,
             year:moduleData.year,
-            semester:moduleData.semester
+            semester:moduleData.semester,
+            credit:moduleData.credit
         }
+        console.log(passData);
         const res = await dispatch(createModules({...passData}));
-        setModuleData({name:'', module_code: '', lecture_in_charge: '', lab_assistant: '',year: '', semester: ''})
+        setModuleData({name:'', module_code: '', lecturer_in_charge: '', lab_assistant: '',year: '', semester: '', credit: ''})
+        if(res.status === 200){
+            message.success("Module Created Successfully")
+        } else {
+            message.error("An Error in Creating a Module")
+        }
+        setLoadingBtn(false);
+
     }
 
 
@@ -93,7 +105,7 @@ const CreateModuleForm = () =>{
                                 options={option_lec}
                                 className="basic-multi-select"
                                 classNamePrefix="select"
-                                onChange={(e) => setModuleData({...moduleData, lecture_in_charge: e.value})}
+                                onChange={(e) => setModuleData({...moduleData, lecturer_in_charge: e.value})}
                             />
                         </div>
                     </div>
@@ -134,6 +146,18 @@ const CreateModuleForm = () =>{
                             onChange={(e) =>setModuleData({...moduleData, semester: e.target.value})}
                         />
                     </div>
+                </div>
+                <div className="mb-3 col">
+                    <label htmlFor="creditLabel" className="form-label">
+                        Credit
+                    </label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="creditLabel"
+                        value={moduleData.credit}
+                        onChange={(e) =>setModuleData({...moduleData, credit: e.target.value})}
+                    />
                 </div>
                 <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
                     Submit
