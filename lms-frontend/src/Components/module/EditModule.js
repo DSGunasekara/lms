4dccf138c3c727d35ebccc96436ef2d94b2e2dad
vidura <laumayangaperera@gmail.com>
2021-09-20 02:06:14 +0530

    add credits

--- lms-frontend/src/Components/module/EditModule.js
+++ lms-frontend/src/Components/module/EditModule.js
@@ -1,218 +1,241 @@
-import React, {useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux';
-import {DatePicker } from "antd";
-import Select from 'react-select';
-import 'antd/dist/antd.css';
-import { Form, Input, Button, message, Skeleton } from 'antd';
-import moment from 'moment';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { DatePicker } from "antd";
+import Select from "react-select";
+import "antd/dist/antd.css";
+import { Form, Input, Button, message, Skeleton } from "antd";
+import moment from "moment";
 
-import { updateSingleModule, getSingleModule } from '../../actions/Modules';
+import { updateSingleModule, getSingleModule } from "../../actions/Modules";
 
-import {getUsers} from "../../actions/Users";
-let option_lec = [], option_lab = [];
+import { getUsers } from "../../actions/Users";
+let option_lec = [],
+  option_lab = [];
 
-const EditSingleModule = ({module}) =>{
+const EditSingleModule = ({ module }) => {
+  const dispatch = useDispatch();
+  let { id } = useParams();
 
+  const [loading, setLoading] = useState(false);
+  const [loadingBtn, setLoadingBtn] = useState(false);
 
-    const dispatch = useDispatch();
-    let { id } = useParams();
+  //initial state of lec and lab
+  const [moduleData, setModuleData] = useState({
+    lecture_in_charge: undefined,
+    lab_assistant: undefined,
+  });
 
-    const [loading, setLoading] = useState(false);
-    const [loadingBtn, setLoadingBtn] = useState(false);
+  //getting single modules data to the fetchUser function
+  const fetchUser = async (modId) => {
+    const res = await dispatch(getSingleModule(modId));
+    console.log(res);
+    const mod = { ...res, year: moment(res.year) };
+    form.setFieldsValue(mod);
+    setModuleData({
+      lecture_in_charge: {
+        value: res?.lecture_in_charge?._id,
+        label: res?.lecture_in_charge?.name,
+      },
+      lab_assistant: {
+        value: res?.lab_assistant?._id,
+        label: res?.lab_assistant?.name,
+      },
+    });
+    setLoading(false);
+  };
 
-
-    //initial state of lec and lab
-    const [moduleData, setModuleData] = useState({
-        lecture_in_charge: undefined,
-        lab_assistant: undefined,
-    })
-
-    //getting single modules data to the fetchUser function
-    const fetchUser = async (modId) =>{
-        const res = await dispatch(getSingleModule(modId));
-        const mod = { ...res, year: moment(res.year)}
-        form.setFieldsValue(mod);
-        setModuleData({
-            lecture_in_charge: {
-                value: res.lecture_in_charge._id,
-                label: res.lecture_in_charge.name
-            },
-            lab_assistant: {
-                value: res.lab_assistant._id,
-                label: res.lab_assistant.name
-            }
-        });
-        setLoading(false);
+  useEffect(() => {
+    if (module) {
+      form.setFieldsValue(module);
+    } else {
+      setLoading(true);
+      fetchUser(id);
     }
+  }, []);
 
-    useEffect( ()=>{
-        if(module){
-            form.setFieldsValue(module)
-        }else {
-            setLoading(true)
-            fetchUser( id )
+  const tailFormItemLayout = {
+    wrapperCol: {
+      xs: {
+        span: 24,
+        offset: 0,
+      },
+      sm: {
+        span: 16,
+        offset: 8,
+      },
+    },
+  };
 
-        }
-    }, [])
+  //calling Users to the Component
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+  const userData = useSelector((state) => state.UserReducer.users);
 
-    const tailFormItemLayout = {
-        wrapperCol: {
-            xs: {
-                span: 24,
-                offset: 0,
-            },
-            sm: {
-                span: 16,
-                offset: 8,
-            },
-        },
+  //filter lecturer from the UserData
+  option_lec = userData
+    ?.filter((user) => user.role === "lecturer")
+    .map((lec) => ({
+      value: lec._id,
+      label: lec.name,
+    }));
+
+  //filter labInstructor from the UserData
+  option_lab = userData
+    ?.filter((user) => user.role === "labInstructor")
+    .map((lab) => ({
+      value: lab._id,
+      label: lab.name,
+    }));
+
+  const [form] = Form.useForm();
+
+  //updating data by passing the new added data
+  const SubmitEdit = async (values) => {
+    setLoadingBtn(true);
+    const updateModule = {
+      id,
+      name: values.name,
+      module_code: values.module_code,
+      year: values.year,
+      semester: values.semester,
+      lecture_in_charge: moduleData.lecture_in_charge.value,
+      lab_assistant: moduleData.lab_assistant.value,
+      credit: values.credit,
     };
-
-    //calling Users to the Component
-    useEffect   (() =>{
-        dispatch(getUsers());
-    }, [dispatch]);
-    const userData = useSelector((state) => state.UserReducer.users)
-
-    //filter lecturer from the UserData
-    option_lec = userData?.filter((user)=> user.role === "lecturer").map((lec) =>({
-        value:lec._id, label: lec.name}))
-
-    //filter labInstructor from the UserData
-    option_lab = userData?.filter((user) => user.role === "labInstructor").map((lab) => ({
-        value: lab._id, label: lab.name}))
-
-    const [form] = Form.useForm();
-
-    //updating data by passing the new added data
-    const SubmitEdit = async (values) =>{
-        setLoadingBtn(true)
-        const updateModule = {
-            id,
-            name: values.name,
-            module_code: values.module_code,
-            year: values.year,
-            semester: values.semester,
-            lecture_in_charge: moduleData.lecture_in_charge.value,
-            lab_assistant:  moduleData.lab_assistant.value
-        }
-       const res = await dispatch(updateSingleModule(updateModule))
-        if(res.status === 200){
-            message.success("Module Updated Successfully")
-        } else {
-            message.error("An Error Occurred")
-        }
-        setLoadingBtn(false);
+    const res = await dispatch(updateSingleModule(updateModule));
+    if (res.status === 200) {
+      message.success("Module Updated Successfully");
+    } else {
+      message.error("An Error Occurred");
     }
+    setLoadingBtn(false);
+  };
 
+  return (
+    <div>
+      {!loading ? (
+        <div style={{ width: "400px", margin: "auto" }}>
+          <h2 style={{ textAlign: "center" }}>Update Module</h2>
+          <Form
+            // {...formItemLayout}
+            layout="vertical"
+            form={form}
+            name="register"
+            onFinish={SubmitEdit}
+            scrollToFirstError
+          >
+            <Form.Item
+              name="name"
+              label="Name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Name!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="module_code"
+              label="Module Code"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Module Code",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-    return(
-        <div>
-            {!loading ? 
-            <div style={{width:'400px', margin: 'auto'}}>
-                <h2 style={{textAlign: 'center'}}>Update Module</h2>
-                <Form
-                    // {...formItemLayout}
-                    layout="vertical"
-                    form={form}
-                    name="register"
-                    onFinish={SubmitEdit}
-                    scrollToFirstError
-                >
-                    <Form.Item
-                        name="name"
-                        label="Name"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Name!',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="module_code"
-                        label="Module Code"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Module Code',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-
-                        <div className="mb-3 col">
-                            <label htmlFor="option_lec" className="form-label">
-                                Lecture In Charge
-                            </label>
-                            <Select
-                                value={moduleData.lecture_in_charge}
-                                name="option_lec"
-                                options={option_lec}
-                                className="basic-multi-select"
-                                style={{width: '100%'}}
-                                onChange={(e) => setModuleData({...moduleData, lecture_in_charge: e})}
-                            />
-                        </div>
-
-                    <div className="mb-3 col">
-                        <label htmlFor="option_lec" className="form-label">
-                            Lab Assistant
-                        </label>
-                        <Select
-                            value={moduleData.lab_assistant}
-                            name="option_lec"
-                            options={option_lab}
-                            className="basic-multi-select"
-                            style={{width: '100%'}}
-                            onChange={(e) => setModuleData({...moduleData, lab_assistant: e})}
-                        />
-                    </div>
-
-                    <Form.Item
-                        name="year"
-                        label="Year"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Semester',
-                            },
-                        ]}
-                    >
-                        <DatePicker picker="year"/>
-                    </Form.Item>
-                    <Form.Item
-                        name="semester"
-                        label="Semester"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Semester',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item {...tailFormItemLayout}>
-                        <Button type="primary" htmlType="submit" loading={loadingBtn}>
-                            Update
-                        </Button>
-                    </Form.Item>
-                </Form>
+            <div className="mb-3 col">
+              <label htmlFor="option_lec" className="form-label">
+                Lecture In Charge
+              </label>
+              <Select
+                value={moduleData.lecture_in_charge}
+                name="option_lec"
+                options={option_lec}
+                className="basic-multi-select"
+                style={{ width: "100%" }}
+                onChange={(e) =>
+                  setModuleData({ ...moduleData, lecture_in_charge: e })
+                }
+              />
             </div>
-        :
-        <> 
-            <Skeleton active/>
-            <Skeleton active/>
-            <Skeleton active/>
-        </>
-        }
+
+            <div className="mb-3 col">
+              <label htmlFor="option_lec" className="form-label">
+                Lab Assistant
+              </label>
+              <Select
+                value={moduleData.lab_assistant}
+                name="option_lec"
+                options={option_lab}
+                className="basic-multi-select"
+                style={{ width: "100%" }}
+                onChange={(e) =>
+                  setModuleData({ ...moduleData, lab_assistant: e })
+                }
+              />
+            </div>
+
+            <Form.Item
+              name="year"
+              label="Year"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Semester",
+                },
+              ]}
+            >
+              <DatePicker picker="year" />
+            </Form.Item>
+            <Form.Item
+              name="semester"
+              label="Semester"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Semester",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="credit"
+              label="credit"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your credit!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item {...tailFormItemLayout}>
+              <Button type="primary" htmlType="submit" loading={loadingBtn}>
+                Update
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
-    )
-}
+      ) : (
+        <>
+          <Skeleton active />
+          <Skeleton active />
+          <Skeleton active />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default EditSingleModule;
--- lms-frontend/src/Components/module/ModuleTable.js
+++ lms-frontend/src/Components/module/ModuleTable.js
@@ -1,160 +1,175 @@
-import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {getModules, removeModule} from "../../actions/Modules";
-import 'antd/dist/antd.css';
-import {Table, Space, Button, Tooltip, message, Popconfirm, Skeleton} from 'antd';
-import {DeleteFilled, EditFilled, EyeFilled, PlusOutlined} from '@ant-design/icons';
-import {useHistory} from "react-router";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getModules, removeModule } from "../../actions/Modules";
+import "antd/dist/antd.css";
+import {
+  Table,
+  Space,
+  Button,
+  Tooltip,
+  message,
+  Popconfirm,
+  Skeleton,
+} from "antd";
+import {
+  DeleteFilled,
+  EditFilled,
+  EyeFilled,
+  PlusOutlined,
+} from "@ant-design/icons";
+import { useHistory } from "react-router";
 
-const ModuleTable = () =>{
+const ModuleTable = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const [module, setModule] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const [module, setModule] = useState([]);
-    const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    dispatch(getModules());
+  }, [dispatch]);
 
-    useEffect(() =>{
-        setLoading(true)
-        dispatch(getModules());
-    }, [dispatch])
+  const moduleData = useSelector((state) => state.ModuleReducer.modules);
 
-    const moduleData = useSelector( (state) => state.ModuleReducer.modules);
+  console.log(moduleData);
 
-    useEffect( ()=>{
-        setModule(moduleData)
-        if (moduleData){
-            setLoading(false)
-        }
-    }, [moduleData])
-
-   const deleteConfirm = async (e) =>{
-      const res = await dispatch(removeModule(e.key));
-      if(res?.status === 200){
-          setModule(module.filter((mod) => mod._id !== e.key))
-          message.success('module Removed');
-      }else {
-          message.error('An Error Occurred');
-      }
-   }
-
-    const editConfirm = (e) =>{
-        history.push(`editModule/${e.key}`)
+  useEffect(() => {
+    setModule(moduleData);
+    if (moduleData) {
+      setLoading(false);
     }
+  }, [moduleData]);
 
-    const SingleModuleLook = (e) =>{
-        history.push(`viewModule/${e.key}`)
+  const deleteConfirm = async (e) => {
+    const res = await dispatch(removeModule(e.key));
+    if (res?.status === 200) {
+      setModule(module.filter((mod) => mod._id !== e.key));
+      message.success("module Removed");
+    } else {
+      message.error("An Error Occurred");
     }
+  };
 
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: 'Module Code',
-            dataIndex: 'module_code',
-            key: 'module_code',
+  const editConfirm = (e) => {
+    history.push(`editModule/${e.key}`);
+  };
 
-        },
-        {
-            title: 'Lecture In Charge',
-            dataIndex: 'lecture_in_charge',
-            key: 'lecture_in_charge',
+  const SingleModuleLook = (e) => {
+    history.push(`viewModule/${e.key}`);
+  };
 
-        },
-        {
-            title: 'Lab Assistant',
-            dataIndex: 'lab_assistant',
-            key: 'lab_assistant',
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Module Code",
+      dataIndex: "module_code",
+      key: "module_code",
+    },
+    {
+      title: "Lecture In Charge",
+      dataIndex: "lecture_in_charge",
+      key: "lecture_in_charge",
+    },
+    {
+      title: "Lab Assistant",
+      dataIndex: "lab_assistant",
+      key: "lab_assistant",
+    },
+    {
+      title: "Year",
+      dataIndex: "year",
+      key: "year",
+    },
+    {
+      title: "Semester",
+      dataIndex: "semester",
+      key: "semester",
+    },
+    {
+      title: "Credits",
+      dataIndex: "credit",
+      key: "credit",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Popconfirm
+            title="Are you sure to delete this Module?"
+            onConfirm={() => deleteConfirm(record)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Tooltip placement="bottom" title="Delete Module">
+              <DeleteFilled />
+            </Tooltip>
+          </Popconfirm>
+          <Tooltip placement="bottom" title="Edit Module">
+            <EditFilled onClick={() => editConfirm(record)} />
+          </Tooltip>
+          <Tooltip placement="bottom" title="View Module">
+            <EyeFilled onClick={() => SingleModuleLook(record)} />
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ];
+  const data = module?.map((mod) => ({
+    key: mod._id,
+    name: mod.name,
+    module_code: mod.module_code,
+    lecture_in_charge: mod.lecture_in_charge?.name,
+    lab_assistant: mod.lab_assistant?.name,
+    year: mod.year.slice(0, 4),
+    semester: mod.semester,
+    credit: mod.credit,
+  }));
 
-        },
-        {
-            title: 'Year',
-            dataIndex: 'year',
-            key: 'year',
+  const newModule = () => {
+    history.push("createModule");
+  };
 
-        },
-        {
-            title: 'Semester',
-            dataIndex: 'semester',
-            key: 'semester',
+  const header = {
+    paddingLeft: 10,
+    fontFamily: "Besley",
+    fontWeight: "bold",
+    paddingTop: 25,
+    paddingBottom: 15,
+  };
 
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: (text, record) => (
-                <Space size="middle">
-                    <Popconfirm
-                        title="Are you sure to delete this Module?"
-                        onConfirm={() => deleteConfirm(record)}
-                        okText="Yes"
-                        cancelText="No"
-                    >
-                        <Tooltip placement="bottom" title="Delete Module">
-                            <DeleteFilled/>
-                        </Tooltip>
-                    </Popconfirm>
-                    <Tooltip placement="bottom" title="Edit Module">
-                        <EditFilled onClick={() => editConfirm(record)} />
-                    </Tooltip>
-                    <Tooltip placement="bottom" title="View Module">
-                        <EyeFilled onClick={() => SingleModuleLook(record)} />
-                    </Tooltip>
-                </Space>
-            ),
-        },
-    ];
-    const data = module?.map((mod) =>({
-        key: mod._id,
-        name: mod.name,
-        module_code: mod.module_code,
-        lecture_in_charge: mod.lecture_in_charge?.name,
-        lab_assistant:mod.lab_assistant?.name,
-        year:mod.year.slice(0, 4),
-        semester:mod.semester
-    }));
-
-    const newModule = () =>{
-        history.push('createModule')
-    }
-
-    const header = {
-        paddingLeft: 10,
-        fontFamily: 'Besley',
-        fontWeight: 'bold',
-        paddingTop: 25,
-        paddingBottom: 15
-    }
-
-    return(
-        <div>
-            {loading ? 
-                <>
-                    <Skeleton active/>
-                    <Skeleton active/>
-                    <Skeleton active/>
-                </>
-            :
-            <>
-                <h3 style={header}>Modules</h3>
-                <Table columns={columns} dataSource={data}/>
-                <Tooltip title="Create New Module">
-                    <Button
-                        type="primary"
-                        shape="circle"
-                        icon={<PlusOutlined />}
-                        size='large'
-                        className="fabBtn"
-                        onClick={newModule}
-                    />
-                </Tooltip>
-            </>
-}
-        </div>
-    )
-}
+  return (
+    <div>
+      {loading ? (
+        <>
+          <Skeleton active />
+          <Skeleton active />
+          <Skeleton active />
+        </>
+      ) : (
+        <>
+          <h3 style={header}>Modules</h3>
+          <Table columns={columns} dataSource={data} />
+          <Tooltip title="Create New Module">
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<PlusOutlined />}
+              size="large"
+              className="fabBtn"
+              onClick={newModule}
+            />
+          </Tooltip>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default ModuleTable;
