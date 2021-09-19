90ec9b1c63e7c390a9913af2ffd25e7bfbaa2052
vidura <laumayangaperera@gmail.com>
2021-09-19 11:39:13 +0530

    ad id

--- lms-backend/Models/TodoList.js
+++ lms-backend/Models/TodoList.js
@@ -1,46 +1,60 @@
 import mongoose from "mongoose";
 
 const TodoListSchema = new mongoose.Schema({
+  CreatedBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
 
-    CreatedBy: {
-        type: mongoose.Types.ObjectId,
-        ref: "User"
+  todo: {
+    items: [
+      {
+        id: {
+          type: String,
+          required: true,
+        },
+
+        name: {
+          type: String,
+          required: true,
+        },
       },
+    ],
+  },
 
-      todo:{
-        items:[
-            {
-                name:{
-                    type:String,
-                    required: true
-                }
-            }
-        ]
+  in_progress: {
+    items: [
+      {
+        id: {
+          type: String,
+          required: true,
+        },
+
+        name: {
+          type: String,
+          required: true,
+        },
       },
+    ],
+  },
 
-      in_progress:{
-        items:[
-            {
-                name:{
-                    type:String,
-                    required: true
-                }
-            }
-        ]
+  done: {
+    items: [
+      {
+        id: {
+          type: String,
+          required: true,
+        },
+
+        name: {
+          type: String,
+          required: true,
+        },
       },
+    ],
+  },
+});
 
-      done:{
-        items:[
-            {
-                name:{
-                    type:String,
-                    required: true
-                }
-            }
-        ]
-      }
-})
-
-const Todo = mongoose.model('TodoList', TodoListSchema);
+const Todo = mongoose.model("TodoList", TodoListSchema);
 
 export default Todo;
--- lms-frontend/src/Components/todoList/TodoList.js
+++ lms-frontend/src/Components/todoList/TodoList.js
@@ -1,152 +1,163 @@
-import React, { useState, useEffect } from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import _ from "lodash";
-import {v4} from "uuid";
+import { v4 } from "uuid";
 
-import {getTask, createTask} from '../../actions/todo';
+import { getTask, createTask } from "../../actions/todo";
 
 import "./todoList.css";
 
-const item ={
-    id:v4(),
-    name: "Clean the house"
-}
+const item = {
+  id: v4(),
+  name: "Clean the house",
+};
 
-const item2 ={
-    id:v4(),
-    name: "wash the car"
-}
+const item2 = {
+  id: v4(),
+  name: "wash the car",
+};
 
-console.log(item)
+console.log(item);
 
-const TodoList = () =>{
+const TodoList = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  // useEffect (() =>{
+  //     dispatch(getTask());
+  // }, [dispatch])
 
-    // useEffect (() =>{
-    //     dispatch(getTask());
-    // }, [dispatch])
+  const [text, setText] = useState("");
+  const [state, setState] = useState({
+    todo: {
+      items: [item],
+    },
+    in_progress: {
+      items: [item2],
+    },
+    done: {
+      items: [],
+    },
+  });
 
-
-    const [text, setText] = useState("")
-    const [state, setState] = useState({
-        "todo": {
-            items:[item]
-        },
-        "in-progress":{
-            items:[item2]
-        },
-        "done":{
-            items:[]
-        }
-    })
-
-    const handleDragEnd = ({destination, source}) =>{
-        if (!destination) {
-            return
-          }
-      
-        if (destination.index === source.index && destination.droppableId === source.droppableId) {
-            return
-        }
-
-        // Creating a copy of item before removing it from state
-        const itemCopy = {...state[source.droppableId].items[source.index]}
-
-
-        setState(prev => {
-            prev = {...prev}
-
-            // Remove from previous items array
-        
-            prev[source.droppableId].items.splice(source.index, 1)
-      
-      
-            // Adding to new items array location
-            prev[destination.droppableId].items.splice(destination.index, 0, itemCopy)
-      
-            return prev
-          })         
+  const handleDragEnd = ({ destination, source }) => {
+    if (!destination) {
+      return;
     }
 
-    const addItem = () => {
-        setState(prev => {
-          return {
-            ...prev,
-            todo: {
-              items: [
-                {
-                  id: v4(),
-                  name: text
-                },
-                ...prev.todo.items
-              ]
-            }
-          }
-        })
-    
-        setText("")
-      }
+    if (
+      destination.index === source.index &&
+      destination.droppableId === source.droppableId
+    ) {
+      return;
+    }
 
-      const save = () =>{
-        dispatch(createTask(state));
-      }
+    // Creating a copy of item before removing it from state
+    const itemCopy = { ...state[source.droppableId].items[source.index] };
 
+    setState((prev) => {
+      prev = { ...prev };
 
-    return(
-        <div className={"todo"}>
-            <div>
-                <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
-                <button onClick={addItem}>Add</button>
+      // Remove from previous items array
+
+      prev[source.droppableId].items.splice(source.index, 1);
+
+      // Adding to new items array location
+      prev[destination.droppableId].items.splice(
+        destination.index,
+        0,
+        itemCopy
+      );
+
+      return prev;
+    });
+  };
+
+  const addItem = () => {
+    setState((prev) => {
+      return {
+        ...prev,
+        todo: {
+          items: [
+            {
+              id: v4(),
+              name: text,
+            },
+            ...prev.todo.items,
+          ],
+        },
+      };
+    });
+
+    setText("");
+  };
+
+  const save = () => {
+    console.log("save", state);
+    dispatch(createTask(state));
+  };
+
+  return (
+    <div className={"todo"}>
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={addItem}>Add</button>
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {_.map(state, (data, key) => {
+          return (
+            <div key={key} className={"column"}>
+              <h3>{data.title}</h3>
+              <Droppable droppableId={key}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={"droppable-col"}
+                    >
+                      {data.items.map((el, index) => {
+                        return (
+                          <Draggable
+                            key={el.id}
+                            index={index}
+                            draggableId={el.id}
+                          >
+                            {(provided, snapshot) => {
+                              return (
+                                <div
+                                  className={`item ${
+                                    snapshot.isDragging && "dragging"
+                                  }`}
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  {el.name}
+                                </div>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
             </div>
-            <DragDropContext onDragEnd={handleDragEnd}>
-                {_.map(state, (data, key) =>{
-                    return(
-                        <div key={key} className={"column"}>
-                            <h3>{data.title}</h3>
-                            <Droppable droppableId={key}>
-                                {(provided, snapshot) =>{
-                                    return(
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.droppableProps}
-                                            className={"droppable-col"}
-                                        >
-                                            {data.items.map((el, index) => {
-                                                return(
-                                                    <Draggable  key={el.id} index={index} draggableId={el.id}>
-                                                        {(provided, snapshot) =>{
-                                                            return(
-                                                                <div
-                                                                className={`item ${snapshot.isDragging && "dragging"}`}
-                                                                ref= {provided.innerRef}
-                                                                {...provided.draggableProps}
-                                                                {...provided.dragHandleProps}
-                                                                >
-                                                                    {el.name}
-                                                                </div>
-                                                            )
-                                                        }}
-                                                    </Draggable >
-                                                )
-                                            })}
-                                             {provided.placeholder}
-                                        </div>
-                                    )
-                                }}
-                            </Droppable>
-                        </div>
-                    )
-                })}
-            </DragDropContext>
-            <div>
-                <button onClick={save}> 
-                    Save
-                </button>
-            </div>
-        </div>
-    )
-}
+          );
+        })}
+      </DragDropContext>
+      <div>
+        <button onClick={save}>Save</button>
+      </div>
+    </div>
+  );
+};
 
 export default TodoList;
