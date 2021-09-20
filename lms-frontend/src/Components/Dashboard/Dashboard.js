4c4466063e27c887ffb8a2f96822f09e0105a33c
Dilain Gunasekara <adsgunasekara18@gmail.com>
2021-09-20 17:06:07 +0530

    Events added to dashboard

--- lms-frontend/src/Components/Dashboard/Dashboard.js
+++ lms-frontend/src/Components/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Carousel, Skeleton, message } from 'antd';
 
 import { getNotices } from '../../actions/Notices';
+import {getEvents} from '../../actions/Events'
 import { getUser, updateUser } from '../../actions/Users';
 import UserModules from '../module/UserModules';
 
@@ -20,41 +21,43 @@ function Dashboard() {
     color: '#fff',
     lineHeight: '100px',
     textAlign: 'center',
-    background: '#364d79',
+    background: '#278ea5',
   };
 
   const dispatch = useDispatch();
   const [user, setUser] = useState([]);
   const [loading, setLoading] = useState(false);
   const noticeData = useSelector((state) => state.NoticeReducer.notices);
+  const eventData = useSelector((state) => state.EventReducer.events);
 
   useEffect(() => {
     setLoading(true);
     dispatch(getNotices());
-    setLoading(false)
+    dispatch(getEvents());
+    setLoading(false);
   }, [dispatch]);
 
   useEffect(() => {
-    getUserData(JSON.parse(localStorage.getItem("profile"))?.payload.user?._id)
-  }, [])
+    getUserData(JSON.parse(localStorage.getItem('profile'))?.payload.user?._id);
+  }, []);
 
-  const getUserData = async(id)=> {
-      setLoading(true)
-      const res = await dispatch(getUser(id))
-      setUser(res);
-      setLoading(false)
-  }
+  const getUserData = async (id) => {
+    setLoading(true);
+    const res = await dispatch(getUser(id));
+    setUser(res);
+    setLoading(false);
+  };
 
-  const unenroll = async(module) => {
-    const newModules= user.modules.filter(mod=> mod.module._id !== module.key)
-    setUser({...user, modules: newModules})
-    const res = await dispatch(updateUser({id: user._id, ...user, modules: newModules}));
+  const unenroll = async (module) => {
+    const newModules = user.modules.filter((mod) => mod.module._id !== module.key);
+    setUser({ ...user, modules: newModules });
+    const res = await dispatch(updateUser({ id: user._id, ...user, modules: newModules }));
     if (res.status === 200) {
-        message.success('Unenrolled from the module');
-    }else {
-        message.error('An Error Occurred');
+      message.success('Unenrolled from the module');
+    } else {
+      message.error('An Error Occurred');
     }
-  }
+  };
   return (
     <div>
       <h3 style={header}>DashBoard</h3>
@@ -65,20 +68,38 @@ function Dashboard() {
           <Skeleton active />
         </>
       ) : (
-          <>
-        <Carousel autoplay>
-          {noticeData?.map((notice) => (
-            <div key={notice._id}>
-              <div style={contentStyle}>
-                 <h3 style={{ color: 'white' }}>{notice.title}</h3>
-                <h6 style={{ color: 'white' }}>{notice.description}</h6>
-              </div>
+        <>
+          <div className="row">
+            <div className="col-6">
+              <h6>Notices</h6>
+              <Carousel autoplay>
+                {noticeData?.map((notice) => (
+                  <div key={notice._id}>
+                    <div style={contentStyle}>
+                      <h3 style={{ color: 'white' }}>{notice.title}</h3>
+                      <h6 style={{ color: 'white' }}>{notice.description}</h6>
+                    </div>
+                  </div>
+                ))}
+              </Carousel>
             </div>
-          ))}
-        </Carousel>
-        <div>
-            <UserModules moduleFilter={user?.modules} unenroll={unenroll}/>
-        </div>
+            <div className="col-6">
+            <h6>Events</h6>
+              <Carousel autoplay>
+                {eventData?.map((event) => (
+                  <div key={event._id}>
+                    <div style={contentStyle}>
+                      <h3 style={{ color: 'white' }}>{event.title}</h3>
+                      <h6 style={{ color: 'white' }}>{event.description}</h6>
+                    </div>
+                  </div>
+                ))}
+              </Carousel>
+            </div>
+          </div>
+          <div>
+            <UserModules moduleFilter={user?.modules} unenroll={unenroll} />
+          </div>
         </>
       )}
     </div>
