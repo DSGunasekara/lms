9e2e5d2c9c2b40645e2a4238b934e0303fdf7e6f
vidura <laumayangaperera@gmail.com>
2021-09-27 22:32:58 +0530

    changes in todolist

--- lms-backend/Routes/todo.js
+++ lms-backend/Routes/todo.js
@@ -6,11 +6,13 @@ import {
   createTask,
   getTasks,
   updateTask,
+  deleteTask,
 } from "../Controllers/todoListController.js";
 
 router.post("/", createTask);
 router.get("/:id", getTask);
 router.get("/", getTasks);
 router.patch("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 export default router;
