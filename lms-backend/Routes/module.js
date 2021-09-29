a412ebbcf39f1792a94d4916df21aba98681261a
vidura <laumayangaperera@gmail.com>
2021-09-29 09:38:16 +0530

    back end  code smell

--- lms-backend/Routes/module.js
+++ lms-backend/Routes/module.js
@@ -1,13 +1,18 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-// import verify from '../Middleware/verify.js'
-import  {getModules, getModule, createModule, updateModule, deleteModule } from '../Controllers/moduleController.js';
+import {
+  getModules,
+  getModule,
+  createModule,
+  updateModule,
+  deleteModule,
+} from "../Controllers/moduleController.js";
 
-router.get('/', getModules);
-router.get('/:id', getModule);
-router.post('/', createModule);
-router.patch('/:id', updateModule);
-router.delete('/:id', deleteModule);
+router.get("/", getModules);
+router.get("/:id", getModule);
+router.post("/", createModule);
+router.patch("/:id", updateModule);
+router.delete("/:id", deleteModule);
 
 export default router;
